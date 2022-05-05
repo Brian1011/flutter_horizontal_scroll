efc7868dc34f3753405d715d1e7c4bc79133858c
Brian Mutinda <mutindabrian26@gmail.com>
2022-05-05 16:40:25 +0300

    add horizontal screen file

---
+++ lib/screens/horizontal_scroll_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HorizontalScrollScreen extends StatefulWidget {
+  const HorizontalScrollScreen({Key? key}) : super(key: key);
+
+  @override
+  _HorizontalScrollScreenState createState() => _HorizontalScrollScreenState();
+}
+
+class _HorizontalScrollScreenState extends State<HorizontalScrollScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
